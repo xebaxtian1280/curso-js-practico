0ca3555236bb109ce37ef7e71936280d3ea16959
xebaxtian1280 <snap.pl1280@gmail.com>
2023-03-15 19:29:33 -0500

    Manipulando el DOM

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
 const parrafito = document.querySelector('.parrafito');
-const pid = document.querySelector('#pid');
+const pid = document.getElementById('pid');
 const input = document.querySelector('input');
 
 console.log(input.value)
@@ -16,3 +16,17 @@ console.log(
     pid,
     input}
 )
+
+//h1.innerText = 'Patito <br> Feo'
+//console.log( h1.setAttribute('class', 'rojo'))
+
+h1.classList.add('rojo')
+h1.classList.remove('verde')
+input.value = 456
+
+const img = document.createElement('img')
+
+img.setAttribute('src', 'https://http2.mlstatic.com/D_NQ_NP_909644-MCO49114589585_022022-V.jpg')
+
+pid.textContent = ""
+pid.innerHTML(img)
